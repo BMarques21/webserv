3386953918b1952170490f3cb2bc2141b98a1742
Vicente Coelho <viceda-s@student.42luxembourg.lu>
2026-02-19 15:34:38 +0100

    Delete wrong project

--- webserver/www/script.js
+++
@@ -1,8 +0,0 @@
-console.log('JavaScript file loaded successfully!');
-
-function testRequest() {
-    fetch('/api/test')
-        .then(response => response.json())
-        .then(data => console.log('API Response:', data))
-        .catch(error => console.error('Error:', error));
-}
